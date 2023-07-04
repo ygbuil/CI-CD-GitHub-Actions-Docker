474d0a453b0c8a34bb85307ee242593334259eee
Llorenç Buil <llorenc.buil@pospotential.com>
2023-07-04 16:53:26 +0200

    test

--- src/main.py
+++ src/main.py
@@ -6,4 +6,10 @@ app = FastAPI()
 def get_hello_world():
     return {'return': 'hello world!'}
 
-# uvicorn src.main:app --port 8000 --reload
+# run normally:
+#   - uvicorn src.main:app --port 8000 --reload
+# run with docker first time:
+#   - docker compose up --build
+#   - docker compose -f docker-compose-prod.yaml up --build
+# run with docker following time:
+#   - docker compose -f docker-compose-prod.yaml up
