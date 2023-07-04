16cb0ccb9ba6d42c939f2b3125241ce5a90aca4e
Llorenç Buil <llorenc.buil@pospotential.com>
2023-07-04 13:30:07 +0200

    test

--- src/main.py
+++ src/main.py
@@ -6,9 +6,4 @@ app = FastAPI()
 def get_hello_world():
     return {'return': 'hello world!'}
 
-# to run normally: 
-#   - uvicorn src.main:app --port 8000 --reload
-# to run using docker (first time): 
-#   - docker compose up --build
-# to run using docker once it is created: 
-#   - docker compose up
+# uvicorn src.main:app --port 8000 --reload
